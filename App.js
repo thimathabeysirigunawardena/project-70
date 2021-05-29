08f5d6575b37a7f2cdda4e61a65318cd46df7727
thimathabeysirigunawardena <71941129+thimathabeysirigunawardena@users.noreply.github.com>
2021-05-28 19:07:02 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
+
+
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+const AppContainer =  createAppContainer(TabNavigator);
